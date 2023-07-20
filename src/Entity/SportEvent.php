0c8e7efb7dacddc0dd996f5b6dae60460c66dd59
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-07-20 22:46:18 +0200

    Revert "Change props to not null - symfony maker bug" - not a bug
    This reverts commit a3db1d35497a5b45079cb92b88f4a73267cc0ff4.

--- src/Entity/SportEvent.php
+++ src/Entity/SportEvent.php
@@ -14,32 +14,32 @@ class SportEvent
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private int $id;
+    private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private string $name;
+    private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    private string $description;
+    private ?string $description = null;
 
     #[ORM\Column]
-    private int $entryFee;
+    private ?int $entryFee = null;
 
     #[ORM\Column]
-    private int $popularityRating;
+    private ?int $popularityRating = null;
 
     #[ORM\Column]
-    private \DateTimeImmutable $createdAt;
+    private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column]
-    private bool $isPublished;
+    private ?bool $isPublished = null;
 
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): string
+    public function getName(): ?string
     {
         return $this->name;
     }
@@ -51,7 +51,7 @@ class SportEvent
         return $this;
     }
 
-    public function getDescription(): string
+    public function getDescription(): ?string
     {
         return $this->description;
     }
@@ -75,7 +75,7 @@ class SportEvent
         return $this;
     }
 
-    public function getPopularityRating(): int
+    public function getPopularityRating(): ?int
     {
         return $this->popularityRating;
     }
@@ -87,7 +87,7 @@ class SportEvent
         return $this;
     }
 
-    public function getCreatedAt(): \DateTimeImmutable
+    public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
     }
