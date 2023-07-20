b5b41d0c923537d76ed79272d06df13a7b2eb368
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-07-20 22:56:35 +0200

    Document properties with comments becouse of OpenApi spec

--- src/Entity/SportEvent.php
+++ src/Entity/SportEvent.php
@@ -11,26 +11,47 @@ use Doctrine\ORM\Mapping as ORM;
 #[ApiResource]
 class SportEvent
 {
+    /**
+     * Identification number of event
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * Name of the sport event
+     */
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    /**
+     * Description of the sport event
+     */
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
+    /**
+     * Amount that each person need to pay to participate in sport event
+     */
     #[ORM\Column]
     private ?int $entryFee = null;
 
+    /**
+     * Popularity rating based on previous impressions of participants form 0 to 10
+     */
     #[ORM\Column]
     private ?int $popularityRating = null;
 
+    /**
+     * Time when event was created
+     */
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
+    /**
+     * Flag that tells is event published
+     */
     #[ORM\Column]
     private ?bool $isPublished = null;
 
