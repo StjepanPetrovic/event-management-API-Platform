0706f383944e5e9693f26c104cf7d85ce2fca714
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-07-20 22:07:17 +0200

    Add entity and repo for SportEvent

---
+++ src/Entity/SportEvent.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SportEventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SportEventRepository::class)]
+class SportEvent
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $entryFee = null;
+
+    #[ORM\Column]
+    private ?int $popularityRating = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEntryFee(): ?int
+    {
+        return $this->entryFee;
+    }
+
+    public function setEntryFee(int $entryFee): static
+    {
+        $this->entryFee = $entryFee;
+
+        return $this;
+    }
+
+    public function getPopularityRating(): ?int
+    {
+        return $this->popularityRating;
+    }
+
+    public function setPopularityRating(int $popularityRating): static
+    {
+        $this->popularityRating = $popularityRating;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
