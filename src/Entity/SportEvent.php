a1fb7bca6356add7c08c7631b1af1984f496ea46
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-07-20 23:07:37 +0200

    Add description on Apiresource

--- src/Entity/SportEvent.php
+++ src/Entity/SportEvent.php
@@ -8,7 +8,9 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SportEventRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    description: 'Event where people can compete in various sports',
+)]
 class SportEvent
 {
     /**
